1cc188e6ecfb69655d819128c7803f860a078b70
nuralamsyah1511-cloud <nur.alamsyah1511@gmail.com>
2026-09-02 21:38:20 +0700

    Create day1.java

---
+++ day1.java
@@ -0,0 +1,14 @@
+public class day1 {
+    public static void main(String[] args) {
+        //ini adalah komen biasa
+
+        /*
+        ini adalah komen multi line
+        */
+
+       /**
+        * ini adalah komen javadoc
+        */
+    }
+    
+}
