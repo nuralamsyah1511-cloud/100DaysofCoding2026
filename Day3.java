5a659e7020a4c918c70265f380d1eb83323ae4b0
nuralamsyah1511-cloud <nur.alamsyah1511@gmail.com>
2026-09-04 15:13:46 +0700

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,13 @@
+public class Day3 {
+    public static void main(String[] args) {
+        System.out.println("Nama saya adalah: Nur Alamsyah");
+
+        System.out.println("Nama saya adalah \nNur Alamsyah");
+
+        System.out.println("Nama saya adalah: \tNur Alamsyah");
+
+        System.out.println("Nama saya adalah: \"Nur Alamsyah\"");
+        
+        System.out.println("Nama saya adalah: \fNur Alamsyah");
+    }    
+}
