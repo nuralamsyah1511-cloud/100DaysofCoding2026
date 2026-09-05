5d67d0203540a9393dd55ef9904dac57ecd267d5
nuralamsyah1511-cloud <nur.alamsyah1511@gmail.com>
2026-09-05 23:13:36 +0700

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,19 @@
+public class Day5 {
+    public static void main(String[] args) {
+        double a = 3.14;
+        float b = 45.89f;
+
+        System.out.println("Nilai dari tipe data double adalah: " + a);
+        System.out.println("Nilai dari tipe data float adalah: " + b + "\n");
+
+        System.out.println("======== Kumpulan Nilai ========");
+
+        System.out.println("Nilai Minimalnya double adalah :" + Double.MIN_VALUE);
+        System.out.println("Nilai Maksimalnya double adalah :" + Double.MAX_VALUE);
+        System.out.println("Nilai Maksimalnya float adalah :" + Float.MIN_VALUE);
+        System.out.println("Nilai Maksimalnya float adalah :" + Float.MAX_VALUE);
+        System.out.println("=================================");
+        
+    }
+    
+}
