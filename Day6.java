d6dc68c421f425158b344daef83c4248a37ba3e4
nuralamsyah1511-cloud <nur.alamsyah1511@gmail.com>
2026-09-07 13:49:32 +0700

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,17 @@
+public class Day6 {
+    public static void main(String[] args) {
+        
+        char a = 'A';
+        char b = 'B';
+        boolean c = true;
+        boolean d = false;
+
+        System.out.println("char dipakai untuk menyimpan satu karakter tunggal, seperti huruf, angka, atau simbol");
+        System.out.printf("Contohnya %s dan %s",a,b);
+
+
+        System.out.println("\nboolean dipakai untuk menyimpan 2 nilai saja yaitu true dan false");
+        System.out.printf("Contohnya %s dan %s",c,d);
+    }
+    
+}
