598366c2f2ec4db35f9f21127e954fa7d4db1bfd
nuralamsyah1511-cloud <nur.alamsyah1511@gmail.com>
2026-09-08 23:04:46 +0700

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,13 @@
+public class Day8 {
+    public static void main(String[] args) {
+
+        int umur;
+        
+        umur = 17;
+        System.out.printf("Pada Tahun 2025 saya berumur %d tahun",umur);
+
+        umur = 18;
+        System.out.printf("%ndan Pada saat Tahun 2026 saya berumur %d tahun",umur);
+    }
+    
+}
