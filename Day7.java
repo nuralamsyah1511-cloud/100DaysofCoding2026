0fbf166dd2606b2e7e322e363942f87b219c3478
nuralamsyah1511-cloud <nur.alamsyah1511@gmail.com>
2026-09-08 20:05:26 +0700

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,12 @@
+public class Day7 {
+    public static void main(String[] args) {
+
+        String prodi = "informatika";
+        String fakultas = "teknik";
+
+        
+        System.out.println("\nProdi saya adalah:"+ prodi);
+        System.out.println("Fakultas saya adalah:"+ fakultas);
+    }
+    
+}
