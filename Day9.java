d7834ce98fdd83119f59f11a89708dbd89e870ad
nuralamsyah1511-cloud <nur.alamsyah1511@gmail.com>
2026-09-10 06:27:36 +0700

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,10 @@
+public class Day9 {
+    public static void main(String[] args) {
+        
+        final int umur = 18;
+        System.out.println("Umur saya sekarang adalah:"+ umur);
+            
+        // data final tidak dapat diubah ketika sudah ditentukan,apabilah diubah akan error
+    }
+    
+}
