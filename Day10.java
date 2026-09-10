05eedf9b599af18bc0b69e20b6ee9e4c2d2d1493
nuralamsyah1511-cloud <nur.alamsyah1511@gmail.com>
2026-09-11 05:54:08 +0700

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,25 @@
+public class Day10 {
+    public static void main(String[] args) {
+
+        System.out.println("========BATAS MINIMAL DAN MAKSIMUN TIPE DATA========");
+        System.out.println("\nTipe data byte");
+        System.out.println("Batas Minimal dan Maksimal tipe data byte adalah: " + Byte.MIN_VALUE+ " dan " + Byte.MAX_VALUE );
+
+        System.out.println("\nTipe data short");
+        System.out.println("Batas Minimal dan Maksimal tipe data short adalah: " + Short.MIN_VALUE+ " dan " + Short.MAX_VALUE );
+
+        System.out.println("\nTipe data int");
+        System.out.println("Batas Minimal dan Maksimal tipe data int adalah: " + Integer.MIN_VALUE+ " dan " + Integer.MAX_VALUE );
+
+        System.out.println("\nTipe data long");
+        System.out.println("Batas Minimal dan Maksimal tipe data long adalah: " + Long.MIN_VALUE+ " dan " + Long.MAX_VALUE );
+
+        System.out.println("\nTipe data float");
+        System.out.println("Batas Minimal dan Maksimal tipe data float adalah: " + Float.MIN_VALUE+ " dan " + Float.MAX_VALUE );
+
+        System.out.println("\nTipt data double");
+        System.out.println("Batas Minimal dan Maksimal tipe data double adalah: " + Double.MIN_VALUE+ " dan " + Double.MAX_VALUE );
+        System.out.println("");
+    }
+    
+}
