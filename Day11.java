9765014c5ec75f7ae481b7072faaf644a6b3c48d
nuralamsyah1511-cloud <nur.alamsyah1511@gmail.com>
2026-09-11 23:46:31 +0700

    Update Day11.java

--- Day11.java
+++ Day11.java
@@ -2,13 +2,14 @@ public class Day11 {
     public static void main(String[] args) {
         java.util.Scanner input = new java.util.Scanner(System.in);
 
+
         System.out.printf("Masukkan nama anda: ");
         String nama = input.nextLine();
         System.out.printf("Masukkan umur anda: ");
         int umur = input.nextInt();
 
-        System.out.printf("Nama anda adalah: %s%nUmur anda adalah: %d tahun",nama,umur);
+        System.out.println("Nama anda adalah : "+nama);
+        System.out.println("Umur anda adalah : "+umur);
     }
-
     
 }
