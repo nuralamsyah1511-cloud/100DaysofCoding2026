7b89c0e968db49ce8d1326407ea936c1c285de78
nuralamsyah1511-cloud <nur.alamsyah1511@gmail.com>
2026-09-15 21:21:59 +0700

    Create Day14.java

---
+++ Day14.java
@@ -0,0 +1,15 @@
+public class Day14 {
+    public static void main(String[] args) {
+        // Day 14
+        // Operasi Aritmatika penjumlahan dan pengurangan
+
+        int a = 30;
+        int b = 10;
+
+        int hasil1 = a+b;
+        System.out.println("Hasil dari a + b = "+hasil1);
+        
+        int hasil2 = a-b;
+        System.out.println("Hasil dari a - b = "+hasil2);
+    }
+}
