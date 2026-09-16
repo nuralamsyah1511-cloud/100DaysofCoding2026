8618ae139bd1887fd1f3edd406ec0ff7434321db
nuralamsyah1511-cloud <nur.alamsyah1511@gmail.com>
2026-09-16 08:51:05 +0700

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,15 @@
+public class Day15 {
+    public static void main(String[] args) {
+        // Day 15
+        // Operasi Aritmatika perkalian dan pembagian
+
+        int a = 50;
+        int b = 25;
+
+        int hasil1 = a*b;
+        System.out.println("Hasil dari a * b = "+hasil1);
+        
+        int hasil2 = a/b;
+        System.out.println("Hasil dari a / b = "+hasil2);
+    }
+}
