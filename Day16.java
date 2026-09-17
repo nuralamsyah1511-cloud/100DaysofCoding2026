48a9f21fef9c5b2d7782644e12449f66958661af
nuralamsyah1511-cloud <nur.alamsyah1511@gmail.com>
2026-09-17 22:30:47 +0700

    Create Day16.java

---
+++ Day16.java
@@ -0,0 +1,16 @@
+public class Day16 {
+    public static void main(String[] args) {
+        //Day 16 
+        //Operasi Aritmetika Modulus atau Sisa Bagi(%)
+
+        int a = 30;
+        int b = 10;
+
+        int hasil1 = a % b;
+        System.out.println("Hasil modulus 30 % 10 adalah:"+hasil1);
+        
+        int hasil2 = b % a;
+        System.out.println("Hasil modulus 10 % 30 adalah:"+hasil2);
+    }
+    
+}
