31f1e0cc63bb2b077b76ccef15a1d02fab842dad
nuralamsyah1511-cloud <nur.alamsyah1511@gmail.com>
2026-09-18 19:10:36 +0700

    Update Day17.java

--- Day17.java
+++ Day17.java
@@ -1,33 +1,24 @@
 public class Day17 {
     public static void main(String[] args) {
         //Day 17
-        //Operator Penguasaan [=,+=,-=,*=,/=,%/]
+        //Operator Penguasaan [=,+=,-=,*=,/=,%=]
 
-        int angka = 10;
+        int angka = 20;
 
         System.out.println("Nilai awal = " + angka);
 
-        // Operator =
-        angka = 20;
-        System.out.println("Setelah angka = 20 : " + angka);
-
-        // Operator +=
         angka += 5;
         System.out.println("Setelah angka += 5 : " + angka);
 
-        // Operator -=
         angka -= 3;
         System.out.println("Setelah angka -= 3 : " + angka);
 
-        // Operator *=
         angka *= 2;
         System.out.println("Setelah angka *= 2 : " + angka);
 
-        // Operator /=
         angka /= 4;
         System.out.println("Setelah angka /= 4 : " + angka);
 
-        // Operator %=
         angka %= 3;
         System.out.println("Setelah angka %= 3 : " + angka);
         
